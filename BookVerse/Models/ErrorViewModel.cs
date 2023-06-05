39d0be4c65fc953586741aa94cd7e496e132f9c7
DKP <iamdkp7531@gmail.com>
2023-06-05 19:49:28 +0530

    Add project files.

---
+++ BookVerse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookVerse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
